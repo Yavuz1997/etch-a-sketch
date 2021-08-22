7dcb9eae4629c08a8464c64839f1e04bff2f01ab
Yavuz <yavuzgogus@gmail.com>
2021-08-22 14:04:38 +0300

    added functional brush color change buttons

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
 let canvas = document.querySelector("#canvas");
 let numberOfCells = 32;
-let side = 500/numberOfCells;
+let side = 700/numberOfCells;
 
+createCells(numberOfCells, side, canvas);
 
-for(let i = 1; i <= numberOfCells ; i++){
-    for(let k = 1; k <= numberOfCells ; k++){
-        const cell = document.createElement("div");
-        cell.classList.add("box");
-        canvas.appendChild(cell);
-        console.log("test");
-
+function createCells(numberOfCells, side, canvas){
+    for(let i = 1; i <= numberOfCells ; i++){
+        for(let k = 1; k <= numberOfCells ; k++){
+            const cell = document.createElement("div");
+            cell.classList.add("box");
+            canvas.appendChild(cell);
+            console.log("creating");
+        }
     }
+    
+    let cells = document.querySelectorAll(".box");
+    
+    cells.forEach((cell)=>{
+        cell.style.width = side +"px";
+        cell.style.height = side +"px";
+    });
 }
+
+
+
+let brush = 1;
+const blackBrush = document.querySelector("#blackBrush");
+blackBrush.addEventListener("click",() => {
+    brush = 0;
+});
+const rainbowBrush = document.querySelector("#rainbowBrush");
+rainbowBrush.addEventListener("click",() => {
+    brush = 1;
+});
 
 let cells = document.querySelectorAll(".box");
 
-cells.forEach((cell)=>{
-    cell.style.width = side +"px";
-    cell.style.height = side +"px";
-});
-
 cells.forEach((cell) => {
-    cell.addEventListener("mouseover",paintRaibow)
+    cell.addEventListener("mouseover",paint)
 });
 
-function paintBlack(e){
-    this.style.background= "black";
+function paint(e){
+    if (brush == 0){
+        this.style.background= "black";     
+    }
+    if (brush == 1){
+        this.style.background= getRandomColor();
+    }
 }
-
-function paintRaibow(e){
-    this.style.background= getRandomColor();
-}
-
 function getRandomColor() {
     let letters = '0123456789ABCDEF';
     let color = '#';
